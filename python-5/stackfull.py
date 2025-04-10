a9fcfd005d126b4dbcb133b144894f512040bc79
beeena4 <febridkune54@gmail.com>
2025-04-10 09:23:58 +0700

    queue

---
+++ python-5/stackfull.py
@@ -0,0 +1,43 @@
+# implementasi stack pada python
+
+# creating a stack
+def creating_stack():
+    stack = []
+    return stack
+
+# creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+# check full
+def check_full(stack):
+    return len(stack) == 10
+
+# adding item into the stack
+def push(stack, item):
+     if check_full(stack):
+        print("stack is full")
+     else:
+        stack.append(item)
+        print("pushed item: " + item)
+
+# removing an element from the stack
+def pop(stack):
+    if(check_empty(stack)):
+        return "stack is empty"
+    return stack.pop()
+
+stack = creating_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+push(stack, str(5))
+push(stack, str(6))
+push(stack, str(7))
+push(stack, str(8))
+push(stack, str(9))
+push(stack, str(10))
+push(stack, str(11))
+print("popped item: " + pop(stack))
+print("stack after popping an element: " + str(stack))
