ca8aad94c46511b59f06f9f1c61167f6685ef879
beeena4 <febridkune54@gmail.com>
2025-04-24 22:00:17 +0700

    implementasi

--- python-6/liniersearch.py
+++ python-6/liniersearch.py
@@ -1,13 +1,23 @@
+# Fungsi untuk mencari bunga berdasarkan nama
 def cari_bunga(daftar_bunga, nama_dicari):
+     # Iterasi (looping) setiap nama bunga dalam daftar
     for bunga in daftar_bunga:
+        # Membandingkan nama bunga dengan input pengguna
         if bunga.lower() == nama_dicari.lower():
-            return True  # Ditemukan
-    return False  # Tidak ditemukan
+            return True  # Ditemukan, kembalikan nilai True
+    return False  # Tidak ditemukan, kembalikan nilai False
 
+# Daftar bunga yang tersedia di toko
 daftar = ["Mawar", "Melati", "Tulip", "Anggrek", "Kamboja"]
+
+# Meminta input dari pengguna untuk nama bunga yang ingin dicari
 nama_input = input("Masukkan nama bunga yang ingin dicari : ")
 
+# Mengecek apakah bunga ada dalam daftar dengan memanggil fungsi cari_bunga dan menampilkan hasil pencarian
 if cari_bunga(daftar, nama_input):
-    print(f"{nama_input} tersedia di toko.")
+    print(f"{nama_input} tersedia di toko.")  # Jika bunga ditemukan
 else:
-    print(f"{nama_input} tidak tersedia.")
+    print(f"{nama_input} tidak tersedia.")  # Jika bunga tidak ditemukan
+
+# Menampilkan Identitas
+print("by : Febriana N.A_006")
